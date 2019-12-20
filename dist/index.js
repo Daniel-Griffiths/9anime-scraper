378a2dec81cd2c2e830a366af39efbf521bd1e8f
Daniel Griffiths <daniel.griffiths.jobs@gmail.com>
2019-12-20 23:56:15 +0000

    Remove dist folder

--- dist/index.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const _9anime_1 = require("./src/9anime");
-exports.searchShows = _9anime_1.searchShows;
-exports.getEpisodes = _9anime_1.getEpisodes;
-exports.getVideo = _9anime_1.getVideo;
