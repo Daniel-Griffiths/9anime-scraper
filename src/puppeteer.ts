330517745ceb07b713c6fc8b72589572c256fcbb
Daniel Griffiths <daniel.griffiths.jobs@gmail.com>
2020-07-02 22:21:45 +0100

    Testing lambda code

--- src/puppeteer.ts
+++ src/puppeteer.ts
@@ -1,65 +1,75 @@
-import puppeteer from "puppeteer";
-import { LaunchOptions } from "puppeteer";
+import chrome from "chrome-aws-lambda";
+import puppeteer from "puppeteer-core";
+import { LaunchOptions } from "puppeteer-core";
 
+import findChrome from "chrome-finder";
 import { IPuppeteerInstance } from "./types";
 
 /**
  * Createa a new Puppeteer instance
  *
- * @param   {LaunchOptions} options
+ * @param   {LaunchOptions} options Specify custom puppeteer options
+ * @param   {boolean} isServerless Required if this runs on a serverless lambda
  * @returns {Promise<IPuppeteerInstance>}
  */
 export const createPuppeteerInstance = async (
-  options: LaunchOptions = {
+  options?: LaunchOptions,
+  isServerless: boolean = false
+): Promise<IPuppeteerInstance> => {
+  const defaultOptions: LaunchOptions = {
     headless: true,
     defaultViewport: null,
-    args: [
-      "--no-pings",
-      "--no-zygote",
-      "--mute-audio",
-      "--no-sandbox",
-      "--disable-sync",
-      "--enable-webgl",
-      "--no-first-run",
-      "--hide-scrollbars",
-      "--disable-breakpad",
-      "--disable-infobars",
-      "--enable-async-dns",
-      "--disable-translate",
-      "--use-mock-keychain",
-      "--disable-extensions",
-      "--disable-speech-api",
-      "--use-gl=swiftshader",
-      "--disable-voice-input",
-      "--disable-cloud-import",
-      "--disable-default-apps",
-      "--disable-hang-monitor",
-      "--disable-wake-on-wifi",
-      "--enable-tcp-fast-open",
-      "--ignore-gpu-blacklist",
-      "--password-store=basic",
-      "--disable-dev-shm-usage",
-      "--disable-notifications",
-      "--disable-print-preview",
-      "--disable-gesture-typing",
-      "--disable-popup-blocking",
-      "--disable-setuid-sandbox",
-      "--metrics-recording-only",
-      "--disable-prompt-on-repost",
-      "--disk-cache-size=33554432",
-      "--no-default-browser-check",
-      "--media-cache-size=33554432",
-      "--enable-simple-cache-backend",
-      "--disable-tab-for-desktop-share",
-      "--prerender-from-omnibox=disabled",
-      "--disable-offer-upload-credit-cards",
-      "--disable-background-timer-throttling",
-      "--disable-client-side-phishing-detection",
-      "--disable-offer-store-unmasked-wallet-cards",
-    ],
-  }
-): Promise<IPuppeteerInstance> => {
-  const browser = await puppeteer.launch(options);
+    ignoreHTTPSErrors: true,
+    executablePath: isServerless ? await chrome.executablePath : findChrome(),
+    args: isServerless
+      ? chrome.args
+      : [
+          "--no-pings",
+          "--no-zygote",
+          "--mute-audio",
+          "--no-sandbox",
+          "--disable-sync",
+          "--enable-webgl",
+          "--no-first-run",
+          "--hide-scrollbars",
+          "--disable-breakpad",
+          "--disable-infobars",
+          "--enable-async-dns",
+          "--disable-translate",
+          "--use-mock-keychain",
+          "--disable-extensions",
+          "--disable-speech-api",
+          "--use-gl=swiftshader",
+          "--disable-voice-input",
+          "--disable-cloud-import",
+          "--disable-default-apps",
+          "--disable-hang-monitor",
+          "--disable-wake-on-wifi",
+          "--enable-tcp-fast-open",
+          "--ignore-gpu-blacklist",
+          "--password-store=basic",
+          "--disable-dev-shm-usage",
+          "--disable-notifications",
+          "--disable-print-preview",
+          "--disable-gesture-typing",
+          "--disable-popup-blocking",
+          "--disable-setuid-sandbox",
+          "--metrics-recording-only",
+          "--disable-prompt-on-repost",
+          "--disk-cache-size=33554432",
+          "--no-default-browser-check",
+          "--media-cache-size=33554432",
+          "--enable-simple-cache-backend",
+          "--disable-tab-for-desktop-share",
+          "--prerender-from-omnibox=disabled",
+          "--disable-offer-upload-credit-cards",
+          "--disable-background-timer-throttling",
+          "--disable-client-side-phishing-detection",
+          "--disable-offer-store-unmasked-wallet-cards",
+        ],
+  };
+
+  const browser = await puppeteer.launch({ ...defaultOptions, ...options });
 
   browser.on("targetcreated", async (target) => {
     const page = await target.page();
